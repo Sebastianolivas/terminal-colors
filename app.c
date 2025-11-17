793dee548881d57f8fb5405b765b90a3c334aeca
Gh0stFr3ak.Optik <114451498+Sebastianolivas@users.noreply.github.com>
2025-11-17 23:35:26 +0000

    Using external libraries

--- app.c
+++ app.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
-// TODO: Add the include directive for the custom library header file here
+#include "term_utils.h"
 
 int main(void) {
     
-    // TODO: Call the function from the library to clear the screen
+    void clear_screen();
     
-    // TODO: Call the function to print a divider line
+    void print_divider();
 
-    // TODO: Set the text color to GREEN using the library function
+    void set_text_color(Color GREEN);
     
     printf("System Status: ONLINE\n");
 
-    // TODO: Set the text color to RED using the library function
+    void set_text_color(Color RED);
     
     printf("Alert: Modular compilation in progress...\n");
 
-    // TODO: Reset the text color back to default
+    void reset_text_color();
     
-    // TODO: Print one last divider line
+    void print_divider();
 
     return 0;
 }
